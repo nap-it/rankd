65bf19f5d85bf86ecdf02f9360ab74d3f9ec6517
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2021-07-26 19:32:39 +0100

    Add options to executable

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,41 @@ mod options;
 mod service;
 
 use failure::Error;
+use slog::{info, o, Drain, Duplicate};
 use std::fs::OpenOptions;
-use slog::{Drain, Duplicate, o, info};
 use structopt::StructOpt;
 use tokio::net::TcpListener;
+use std::net::Ipv4Addr;
 
 #[tokio::main]
 async fn main() {
     // Read options from CLI.
     let cli_options = options::RankdOpts::from_args();
-    //let config = config::parse_file(&cli_options.config_file);
+
+    // Assign default values to CLI options.
+    let mut log_file: String = String::from("/var/log/rankd.log");
+    let mut quiet: bool = false;
+    let mut server_ip: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    let mut running_deployment_name: String = String::from("Default configuration");
+
+    match &cli_options.config_file {
+        Some(file) => {}
+        None => {}
+    }
+
+    let config = config::parse_file(&cli_options.config_file);
 
     // Initialize logger.
-    let log_file = OpenOptions::new().create(true).write(true).truncate(true).open("rankd.log").unwrap();
+    let log_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("rankd.log")
+        .unwrap();
     let terminal_decorator = slog_term::TermDecorator::new().build();
-    let terminal_drain = slog_term::FullFormat::new(terminal_decorator).build().fuse();
+    let terminal_drain = slog_term::FullFormat::new(terminal_decorator)
+        .build()
+        .fuse();
     let terminal_drain = slog_async::Async::new(terminal_drain).build().fuse();
     let file_decorator = slog_term::PlainDecorator::new(log_file);
     let file_drain = slog_term::FullFormat::new(file_decorator).build().fuse();
--- src/options.rs
+++ src/options.rs
@@ -4,14 +4,28 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "rankd")]
 pub struct RankdOpts {
-    //#[structopt(
-    //    parse(from_os_str),
-    //    short = "c",
-    //    long = "config",
-    //    value_name = "filename",
-    //    help = "configuration file"
-    //)]
-    //pub config_file: PathBuf,
+    #[structopt(
+        parse(from_os_str),
+        short = "c",
+        long = "config",
+        value_name = "filename",
+        help = "configuration file"
+    )]
+    pub config_file: Option<PathBuf>,
+    #[structopt(
+        short = "l",
+        long = "log-file",
+        value_name = "logfile",
+        help = "log file location (default is /var/logs/rankd.log)"
+    )]
+    pub log_file: Option<PathBuf>,
+    #[structopt(
+        short = "v",
+        long = "verbose",
+        value_name = "verbose",
+        help = "verbosity level (v - info, vv - debug, vvv - trace)"
+    )]
+    pub verbose: bool,
     #[structopt(
         short = "q",
         long = "quiet",
