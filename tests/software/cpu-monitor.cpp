bdd03d1220bd08f19a2f723c139a7002d22e41ad
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2022-04-19 10:06:49 +0100

    Add new tests to the cpu-monitor app

--- tests/software/cpu-monitor.cpp
+++ tests/software/cpu-monitor.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <thread>
 
-#include "structs/cpu.h"
+#include "comp-lib.h"
 
 using namespace std;
 
@@ -10,16 +10,26 @@ int main() {
   cpu.snap();
 
   cout << "Details on the current CPU:" << endl;
-  cout << "Vendor ID: " << cpu.vendor_id().value() << endl;
-  cout << "Model Name: " << cpu.model().value() << endl;
+  cout << "Vendor ID: " << cpu.vendor_id() << endl;
+  cout << "Model Name: " << cpu.model_name() << endl;
+  cout << "Model: " << cpu.model() << endl;
+  cout << "Family: " << cpu.family() << endl;
+  cout << "Stepping: " << cpu.stepping() << endl;
+  cout << "Microcode: " << cpu.microcode() << endl;
+  cout << "Flags: ";
+  for (auto& flag : cpu.flags()) {
+    cout << flag << ", ";
+  }
+  cout << endl;
 
   cout << "CPU stats are: (System/User/Nice/IRQs/SoftIRQs/IOwait/IDLE/Steal)"
        << endl;
   while (true) {
-    cout << "· " << cpu.snapshot().system << "/" << cpu.snapshot().user << "/"
-         << cpu.snapshot().nice << "/" << cpu.snapshot().irqs << "/"
-         << cpu.snapshot().softirqs << "/" << cpu.snapshot().iowait << "/"
-         << cpu.snapshot().idle << "/" << cpu.snapshot().steal << endl;
+    //cout << "· " << cpu.snapshot().system << "/" << cpu.snapshot().user << "/"
+    //     << cpu.snapshot().nice << "/" << cpu.snapshot().irqs << "/"
+    //     << cpu.snapshot().softirqs << "/" << cpu.snapshot().iowait << "/"
+    //     << cpu.snapshot().idle << "/" << cpu.snapshot().steal << endl;
+    cout << (cpu.snapshot().in_use * 1000000)/2.4 << " %" << endl;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     cpu.snap();
