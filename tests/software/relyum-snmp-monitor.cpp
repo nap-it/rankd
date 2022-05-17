01f1fe549e776e7ec8d41330a3b8aab72413b9a2
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2022-05-17 17:52:47 +0100

    Add some turnarounds in testing software for rankd-time

--- tests/software/relyum-snmp-monitor.cpp
+++ tests/software/relyum-snmp-monitor.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
 
+#include <string>
+
 #include <net-snmp/net-snmp-config.h>
+#include <net-snmp/utilities.h>
 #include <net-snmp/net-snmp-includes.h>
+
+#define NETSNMP_DS_APP_DONT_FIX_PDUS 0
 
 using namespace std;
 
 int main() {
   // Variable declaration.
-  struct snmp_session session;
-  struct snmp_session* snmp_session;
-  struct snmp_pdu* pdu;
-  struct snmp_pdu* response;
+  netsnmp_session session;
+  netsnmp_session*ss;
+  netsnmp_pdu* pdu;
+  netsnmp_pdu* response;
 
   oid OID[MAX_OID_LEN];
-  size_t OID_length = MAX_OID_LEN;
+  size_t OID_length;
 
-  struct variable_list* variables;
+  netsnmp_variable_list* vars;
   int status;
 
+  SOCK_STARTUP;
+
   // Initialize the SNMP library.
-  cout << "Preparing to start." << endl;
-  init_snmp("rankd-time");
+  //cout << "Preparing to start." << endl;
+  init_snmp("snmpapp");
   snmp_sess_init(&session); // this sets up the default values in.
   session.version = SNMP_VERSION_1;
   session.peername = "localhost";
   session.community = (u_char*)"public";
   session.community_len = strlen(reinterpret_cast<const char *>(session.community));
-  cout << "Starting SNMP session with host " << session.peername << endl;
+  //cout << "Starting SNMP session with host " << session.peername << endl;
 
   // Open the session.
-  snmp_session = snmp_open(&session);
-  if (!snmp_session) {
+  ss = snmp_open(&session);
+  if (!ss) {
     snmp_perror("ACK");
     snmp_log(LOG_ERR, "Something happened\n");
     exit(2);
@@ -38,29 +45,53 @@ int main() {
 
   // Create a PDU for our request.
   pdu = snmp_pdu_create(SNMP_MSG_GET);
-  //read_objid("SOCE-DESIGN-3524::rxPreemptionStartFrame.0", OID, &OID_length);
-  get_node("SOCE-DESIGN-3524-MIB::rxPreemptionStartFrame.0", OID, &OID_length);
-  cout << "SOCE-DESIGN-3524-MIB::rxPreemptionStartFrame.0" << endl;
+  OID_length = MAX_OID_LEN;
+  //read_objid(".1.3.6.1.4.1.47561.3524.2.6.21.0", OID, &OID_length);
+  status = get_node("SOCE-DESIGN-3524-MIB::PORT-1.phySpeed.0", OID, &OID_length);
+  if (!status) {
+    snmp_perror("Bad OID");
+  }
+  cout << "OID has now a value of " << OID_length << endl;
+  //cout << "SOCE-DESIGN-3524-MIB::PORT-1.phySpeed.0" << endl;
   snmp_add_null_var(pdu, OID, OID_length);
-  status = snmp_synch_response(snmp_session, pdu, &response);
-  if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
-    cout << "Got a successful answer" << endl;
-    for (variables = response->variables; variables != nullptr; variables = variables->next_variable) {
-      cout << "Variable " << variables->val.integer << " is being accessed." << endl;
-      //print_variable(variables->name, variables->name_length, variables);
+
+  status = snmp_synch_response(ss, pdu, &response);
+  if (status == STAT_SUCCESS) {
+    if (response->errstat == SNMP_ERR_NOERROR) {
+      cout << "Got a successful answer" << endl;
+      cout << "Rid: " << response->reqid << endl;
+      cout << "Leng: " << response->variables->val_len << endl;
+      cout << "Request name: ";
+      for (int i = 0; i != OID_length; i++) {
+        cout << "." << OID[i];
+      }
+      cout << endl;
+      cout << "Response name: ";
+      for (int i = 0; i != 12; i++) {
+        cout << "." << response->variables->name[i];
+      }
+      cout << endl;
+      for (vars = response->variables; vars; vars = vars->next_variable) {
+        print_variable(vars->name, vars->name_length, vars);
+        cout << "Variable " << *vars->val.integer << " is being accessed."
+             << endl;
+
+      }
     }
   } else {
     if (status == STAT_SUCCESS) {
       cout << "Error in packet" << endl << "Reason is " << snmp_errstring(response->errstat) << endl;
     } else {
-      snmp_sess_perror("snmpget", snmp_session);
+      snmp_sess_perror("snmpget", ss);
     }
   }
 
   if (response) {
     snmp_free_pdu(response);
   }
-  snmp_close(snmp_session);
+  snmp_close(ss);
+
+  SOCK_CLEANUP;
 
   return 0;
 }
