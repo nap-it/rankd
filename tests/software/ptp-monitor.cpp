b2f500855af96be3fa4b8468001c69e937eb8cae
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2022-11-17 15:59:37 +0000

    Add new ptp-monitor test to tests/software

--- tests/software/ptp-monitor.cpp
+++ tests/software/ptp-monitor.cpp
@@ -4,7 +4,7 @@
 #include "time-lib.h"
 
 int main() {
-  auto info = get_ptp_capabilities("enp2s0");
+  auto info = get_ptp_capabilities("eth0");
 
   for (auto [key, value] : info) {
     std::cout << key << ":   (with " << value.size() << " values)" << std::endl;
@@ -12,6 +12,21 @@ int main() {
       std::cout << "\t" << element << std::endl;
     }
   }
+
+  std::cout << "The device has capabilities of PTP? ";
+  auto has = capability_level_in(info);
+  switch (has) {
+  case PTPCapability::HARDWARE:
+    std::cout << "Hardware" << std::endl; break;
+  case PTPCapability::SOFTWARE_ONLY:
+    std::cout << "Software" << std::endl; break;
+  case PTPCapability::UNCAPABLE:
+    std::cout << "No" << std::endl; break;
+  }
+
+  std::cout << std::endl;
+
+  debug_all_interfaces_ptp_capabilities();
 
   return 0;
 }
