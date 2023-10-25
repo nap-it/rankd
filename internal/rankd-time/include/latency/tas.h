c0a6736f1ff4445140b2d0e663053ecdf143ff99
Rui Lopes <ruieduardo.fa.lopes@icloud.com>
2023-10-26 00:41:58 +0100

    Set check_identity() in TAS of internals/rankd-time TC's helper

--- internal/rankd-time/include/latency/tas.h
+++ internal/rankd-time/include/latency/tas.h
@@ -1,8 +1,16 @@
 #ifndef RANKD_TIME_LAT_TAS_H
 #define RANKD_TIME_LAT_TAS_H
 
+#include <cstdint>
+#include <cstring>
+
+#include <linux/rtnetlink.h>
+
 class TASEntry {
 public:
+    static bool check_identity(const struct rtattr* attribute) {
+        return attribute != nullptr && strcmp(static_cast<const char*>(RTA_DATA(attribute)), "taprio") == 0;
+    }
 
 private:
 
