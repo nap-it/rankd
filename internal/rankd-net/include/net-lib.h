5500c7454a19a3f51de1503613a39ca7922a6110
Rui Lopes <ruieduardo.fa.lopes@icloud.com>
2024-02-18 01:02:04 +0000

    Decouple yanpit from rankd

--- internal/rankd-net/include/net-lib.h
+++ internal/rankd-net/include/net-lib.h
@@ -1,7 +1,7 @@
 #ifndef RANKD_NET_LIB_H
 #define RANKD_NET_LIB_H
 
-#include "data-sources/yanpit.h"
+//#include "data-sources/yanpit.h"
 #include "data-sources/interfaces.h"
 #include "data-sources/neighbors.h"
 #include "data-sources/routes.h"
