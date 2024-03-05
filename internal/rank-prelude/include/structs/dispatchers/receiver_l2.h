16f208013127eeb63f7912b0a4d8a0bb73f855f7
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2024-03-05 19:48:21 +0000

    Add raw socket handling as RawReceiverL2 in dispatchers/receiver_l2

--- internal/rank-prelude/include/structs/dispatchers/receiver_l2.h
+++ internal/rank-prelude/include/structs/dispatchers/receiver_l2.h
@@ -6,7 +6,40 @@
 #include <thread>
 #include <vector>
 
+#include <unistd.h>
+#include <linux/if.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
 #include "structs/message.h"
+
+#define RANK_INTERFACE "rank0" // TODO Change this value or allow this value to be configured.
+#define TEMPORARY_BUFFER_LENGTH 65536
+
+class RawReceiverL2 {
+public:
+    static RawReceiverL2* get_instance() {
+        static RawReceiverL2 instance;
+        return &instance;
+    }
+    RawReceiverL2* set_queue(std::queue<std::vector<uint8_t>>* queue, std::mutex* queue_mutex);
+    RawReceiverL2* execute();
+    RawReceiverL2* stop();
+    bool is_running();
+    void operator()();
+    ~RawReceiverL2();
+private:
+    RawReceiverL2();
+    bool _running = false;
+    int _raw_socket;
+    unsigned char* _temporary_buffer;
+    std::thread _thread;
+    std::queue<std::vector<uint8_t>>* _queue;
+    std::mutex* _queue_mutex;
+};
 
 class ReceiverL2 {
 public:
@@ -28,6 +61,7 @@ private:
     std::queue<std::vector<uint8_t>> _queue{};
     std::queue<Message*>* _message_deposit = nullptr;
     std::mutex* _message_deposit_mutex;
+    std::mutex* _queue_mutex;
 };
 
 #endif //RANK_PRELUDE_DISPATCHER_RECEIVER_L2_H
--- internal/rank-prelude/structs/dispatchers/receiver_l2.cpp
+++ internal/rank-prelude/structs/dispatchers/receiver_l2.cpp
@@ -1,5 +1,105 @@
 #include "structs/dispatchers/receiver_l2.h"
 
+RawReceiverL2 *RawReceiverL2::set_queue(std::queue<std::vector<uint8_t>> *queue, std::mutex *queue_mutex) {
+    _queue = queue;
+    _queue_mutex = queue_mutex;
+
+    return this;
+}
+
+RawReceiverL2 *RawReceiverL2::execute() {
+    if (_running) {
+        return this;
+    }
+
+    _running = true;
+    _thread = std::thread(std::ref(*this));
+
+    return this;
+}
+
+RawReceiverL2 *RawReceiverL2::stop() {
+    if (not _running) {
+        return this;
+    }
+
+    _running = false;
+    _thread.join();
+
+    return this;
+}
+
+bool RawReceiverL2::is_running() {
+    return _running;
+}
+
+RawReceiverL2::RawReceiverL2() {
+    // Create the raw socket.
+    _raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (_raw_socket < 0) {
+        throw std::exception(); // TODO Handle this error. This needs high privileges.
+    }
+
+    // Get the interface index.
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(struct ifreq));
+    strncpy(ifr.ifr_name, reinterpret_cast<const char*>(RANK_INTERFACE), sizeof(ifr.ifr_name));
+    int error_code = ioctl(_raw_socket, SIOCGIFINDEX, &ifr);
+    if (error_code < 0) {
+        throw std::exception(); // TODO Handle this error.
+    }
+    int interface_index = ifr.ifr_ifindex;
+
+    // Bind raw socket to given network interface.
+    struct sockaddr_ll sll;
+    memset(&sll, 0, sizeof(struct sockaddr_ll));
+    sll.sll_family = AF_PACKET;
+    sll.sll_ifindex = interface_index;
+    sll.sll_protocol = ETH_P_ALL;
+    error_code = bind(_raw_socket, (struct sockaddr*)&sll, sizeof(struct sockaddr_ll));
+    if (error_code == -1) {
+        throw std::exception(); // TODO Handle this error.
+    }
+
+    // Initialize temporary buffer.
+    _temporary_buffer = (unsigned char*) malloc(TEMPORARY_BUFFER_LENGTH);
+}
+
+RawReceiverL2::~RawReceiverL2() {
+    // Free allocated memory.
+    free(_temporary_buffer);
+
+    // Close opened raw socket.
+    close(_raw_socket);
+}
+
+void RawReceiverL2::operator()() {
+    while (_running) {
+        // Clear the temporary buffer completely.
+        memset(_temporary_buffer, 0, TEMPORARY_BUFFER_LENGTH);
+
+        // Wait for another frame to be received. This method will be sensible to all Ethernet frames!
+        int received_bytes = recv(_raw_socket, _temporary_buffer, TEMPORARY_BUFFER_LENGTH, 0);
+        if (received_bytes < 0) {
+            continue;
+        }
+
+        // Parse the received bytes as an Ethernet header.
+        struct ethhdr* ethernet_header = (struct ethhdr*) _temporary_buffer;
+        if (ntohs(ethernet_header->h_proto) == RANK_ETHERTYPE) {
+            std::vector<uint8_t> data;
+            uint8_t* rank_data = _temporary_buffer + sizeof(struct ethhdr);
+            size_t rank_data_length = received_bytes - sizeof(struct ethhdr);
+
+            // Insert the sliced C-array into the newly created vector.
+            data.insert(data.end(), &rank_data[0], &rank_data[rank_data_length]);
+
+            std::lock_guard<std::mutex> guard(*_queue_mutex);
+            _queue->push(data);
+        }
+    }
+}
+
 void ReceiverL2::enqueue_bytes(const std::vector<uint8_t> &bytes) {
     _queue.push(bytes);
 }
@@ -38,8 +138,12 @@ bool ReceiverL2::is_running() {
 void ReceiverL2::operator()() {
     while (_running) {
         if (not _queue.empty()) {
-            auto bytes = _queue.front();
-            _queue.pop();
+            std::vector<uint8_t> bytes;
+            {
+                std::lock_guard<std::mutex> guard(*_queue_mutex);
+                bytes = _queue.front();
+                _queue.pop();
+            }
 
             // TODO Parse the "bytes" into a message.
             Message* message = nullptr;
