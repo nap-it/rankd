db3fa1e0ee7c2298628c8502956d85e3123a485b
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2024-10-10 16:57:44 +0100

    Add FROM_SIMUZILLA flag to internals/ and rank-prelude/ cmake

---
+++ internal/dry-rankd-comp/include/dry-comp-lib.h
@@ -0,0 +1,10 @@
+#ifndef DRYRANKD_COMP_LIB_H
+#define DRYRANKD_COMP_LIB_H
+
+#include "data-sources/cpu.h"
+#include "data-sources/memory.h"
+#include "data-sources/os.h"
+#include "data-sources/sensors.h"
+#include "data-sources/storage.h"
+
+#endif // DRYRANKD_COMP_LIB_H
